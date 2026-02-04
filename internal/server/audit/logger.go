dffc06ba82da7f0cace3fd7bc24ffc972711dcc7
ashershnyov <antshersh@gmail.com>
2026-02-04 20:51:40 +0300

    iter17: added pool for audit json encoder buffer

--- internal/server/audit/logger.go
+++ internal/server/audit/logger.go
@@ -1,14 +1,36 @@
 package audit
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
+	"sync"
 	"time"
 
 	"github.com/ashershnyov/go-metrics-gatherer/internal/server/config"
 	"golang.org/x/sync/errgroup"
 )
+
+var bufferPool = sync.Pool{
+	New: func() interface{} {
+		return &bytes.Buffer{}
+	},
+}
+
+func getBuffer() *bytes.Buffer {
+	buf := bufferPool.Get().(*bytes.Buffer)
+	buf.Reset()
+	return buf
+}
+
+func putBuffer(buf *bytes.Buffer) {
+	// shouldn't put buffers with huge capacities to the pool
+	if buf.Cap() > 4096 {
+		return
+	}
+	bufferPool.Put(buf)
+}
 
 // Destination defines the destination to write audit log to.
 type Destination interface {
@@ -37,8 +59,11 @@ func (l *Logger) Log(ctx context.Context, metrics []string, ipAddress string) er
 		IPAddress: ipAddress,
 	}
 
-	bytes, err := json.Marshal(record)
-	if err != nil {
+	buf := getBuffer()
+	defer putBuffer(buf)
+	encoder := json.NewEncoder(buf)
+
+	if err := encoder.Encode(record); err != nil {
 		return fmt.Errorf("error marshalling audit record: %w", err)
 	}
 
@@ -48,7 +73,7 @@ func (l *Logger) Log(ctx context.Context, metrics []string, ipAddress string) er
 			continue
 		}
 		eg.Go(func() error {
-			return dst.Log(ctx, bytes)
+			return dst.Log(ctx, bytes.TrimSpace(buf.Bytes()))
 		})
 	}
 
