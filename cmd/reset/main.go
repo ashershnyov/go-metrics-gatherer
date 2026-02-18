4cb3f837f1ed003379ea1ccc79df32930da63a9f
ashershnyov <antshersh@gmail.com>
2026-02-18 22:51:13 +0300

    iter21: resetgen init

---
+++ cmd/reset/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"path/filepath"
+	"runtime"
+
+	"github.com/ashershnyov/go-metrics-gatherer/internal/resetgen"
+)
+
+var ignoreSuffixes = []string{
+	"_test.go",
+	"_mock.go",
+	".gen.go",
+}
+
+func main() {
+	_, currentFile, _, ok := runtime.Caller(0)
+	if !ok {
+		panic("failed to get current file path")
+	}
+	root := filepath.Dir(filepath.Dir(currentFile))
+	err := resetgen.Do(root, ignoreSuffixes, nil)
+	fmt.Println(err)
+}
